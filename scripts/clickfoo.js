ccf24b223a3ff9e7e918f1e48936a28f0055e087
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-02 18:39:56 +0300

    ыполненное задание урока 1

--- scripts/clickfoo.js
+++ scripts/clickfoo.js
@@ -1,4 +1,20 @@
+let money;
+let income;
+let addExpenses;
+let mission;
+let period;
+let deposit;
+
+
+
 function func(){
-    alert('hello world');
+    money = parseInt(document.getElementById("money").value);
+    income = parseInt(document.getElementById("income").value);
+    addExpenses = parseInt(document.getElementById("addExpenses").value);
+    mission = document.getElementById("mission").value;
+    period = document.getElementById("period").value;
+    deposit = (money+income)-addExpenses;
+    alert("За период " + period + " дней вы потратили на "+ mission +" " + addExpenses +
+    ". Остаток на вашем счёте составляет " + deposit);
     console.log('logged');
 }
