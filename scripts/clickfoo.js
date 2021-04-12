555b81842f43b38ce09649d09610e0c01abd41af
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-13 00:39:55 +0300

    выполненный урок 2

--- scripts/clickfoo.js
+++ scripts/clickfoo.js
@@ -7,14 +7,36 @@ let deposit;
 
 
 
-function func(){
-    money = parseInt(document.getElementById("money").value);
-    income = parseInt(document.getElementById("income").value);
-    addExpenses = parseInt(document.getElementById("addExpenses").value);
-    mission = document.getElementById("mission").value;
-    period = document.getElementById("period").value;
-    deposit = (money+income)-addExpenses;
-    alert("За период " + period + " дней вы потратили на "+ mission +" " + addExpenses +
-    ". Остаток на вашем счёте составляет " + deposit);
-    console.log('logged');
+money = 350000;
+income = "фриланс";
+addExpenses = "Бензин, еДа, вещи, собака, развлечение";
+deposit = true;
+mission = 5560000;
+period = 8;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+
+let myArr = addExpenses.toLowerCase().split(", ");
+
+console.log(myArr);
+
+let budgetDay = money/30;
+
+console.log(budgetDay);
+
+/* Для усложнённого задания
+var num = 266219;
+var digits = num.toString().split('');
+var realDigits = digits.map(Number)
+let summary = 1;
+for(i=0; i<realDigits.length;i++){
+    summary *= realDigits[i];
 }
+console.log(summary);
+sumpow = summary**3;
+console.log(sumpow.toString().substring(0, 2));*/
