ec84456447bfdbc53b57d63d75732e6dc0624cf0
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-13 20:32:14 +0300

    Объявление объединил с инициализацией

--- scripts/clickfoo.js
+++ scripts/clickfoo.js
@@ -1,18 +1,9 @@
-let money;
-let income;
-let addExpenses;
-let mission;
-let period;
-let deposit;
-
-
-
-money = 350000;
-income = "фриланс";
-addExpenses = "Бензин, еДа, вещи, собака, развлечение";
-deposit = true;
-mission = 5560000;
-period = 8;
+let money = 350000;
+let income = "фриланс";
+let addExpenses = "Бензин, еДа, вещи, собака, развлечение";
+let mission = 5560000;
+let period = 8;
+let deposit = true;
 
 console.log(typeof money);
 console.log(typeof income);
@@ -29,14 +20,4 @@ let budgetDay = money/30;
 
 console.log(budgetDay);
 
-/* Для усложнённого задания
-var num = 266219;
-var digits = num.toString().split('');
-var realDigits = digits.map(Number)
-let summary = 1;
-for(i=0; i<realDigits.length;i++){
-    summary *= realDigits[i];
-}
-console.log(summary);
-sumpow = summary**3;
-console.log(sumpow.toString().substring(0, 2));*/
+
