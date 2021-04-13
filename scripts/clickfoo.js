5805eb02144698012f2d852eaf09c206f315c799
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-14 00:45:35 +0300

    Get back code from lesson 02

--- scripts/clickfoo.js
+++ scripts/clickfoo.js
@@ -7,20 +7,30 @@ let mission = 5560000;
 let period = 8;
 let deposit = confirm("Есть ли у Вас депозит в банке?");
 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses);
+let myArr = addExpenses.toLowerCase().split(", ");
+console.log(myArr);
+
 let expenses1 = prompt("Укажите обязательную статью расходов");
 let amount1 = +prompt("Во сколько это обойдётся?"); 
-let expenses2 = prompt("Укажите обязательную статью расходов");;
+let expenses2 = prompt("Укажите ещё одну обязательную статью расходов");;
 let amount2 = +prompt("Во сколько это обойдётся?"); ;
 
 let budgetMonth = money - (amount1 + amount2);
-console.log(`BudgetMonth: ${budgetMonth}`);
+console.log(`Бюджет на месяц: ${budgetMonth}`);
 
 period = Math.ceil(mission/budgetMonth);
-console.log(`period: ${period}`)
+console.log(`Цель будет достигнута за ${period} месяцев(-а)`)
 
 
 let budgetDay = budgetMonth/30;
-console.log(`budgetDay: ${Math.floor(budgetDay)}`);
+console.log(`Бюджет на день: ${Math.floor(budgetDay)}`);
 
 let incomelvl;
 if (budgetDay>=1200){
@@ -35,5 +45,5 @@ else if(budgetDay>=0){
 else{
     incomelvl="что-то пошло не так";
 }
-alert(incomelvl);
+console.log(incomelvl);
 
