499edf1c2e501bae1b5390653170caa155936521
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-14 00:00:42 +0300

    Выполнил 3йй урок

--- scripts/clickfoo.js
+++ scripts/clickfoo.js
@@ -1,23 +1,39 @@
-let money = 350000;
+'use strict'
+
+let money = +prompt("Ваш месячный доход");
 let income = "фриланс";
-let addExpenses = "Бензин, еДа, вещи, собака, развлечение";
+let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
 let mission = 5560000;
 let period = 8;
-let deposit = true;
+let deposit = confirm("Есть ли у Вас депозит в банке?");
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
+let expenses1 = prompt("Укажите обязательную статью расходов");
+let amount1 = +prompt("Во сколько это обойдётся?"); 
+let expenses2 = prompt("Укажите обязательную статью расходов");;
+let amount2 = +prompt("Во сколько это обойдётся?"); ;
 
-let myArr = addExpenses.toLowerCase().split(", ");
+let budgetMonth = money - (amount1 + amount2);
+console.log(`BudgetMonth: ${budgetMonth}`);
 
-console.log(myArr);
+period = Math.ceil(mission/budgetMonth);
+console.log(`period: ${period}`)
 
-let budgetDay = money/30;
 
-console.log(budgetDay);
+let budgetDay = budgetMonth/30;
+console.log(`budgetDay: ${Math.floor(budgetDay)}`);
 
+let incomelvl;
+if (budgetDay>=1200){
+    incomelvl="У вас высокий уровень дохода";
+}
+else if(budgetDay>=600){
+    incomelvl="У вас средний уровень дохода";
+}
+else if(budgetDay>=0){
+    incomelvl="К сожалению у Вас уровень дохода ниже среднего";
+}
+else{
+    incomelvl="что-то пошло не так";
+}
+alert(incomelvl);
 
